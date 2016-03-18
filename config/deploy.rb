d2ec6c70f977408dbbe9e99eddaf16b3415b7e50
Raymond Usbal <rsusbal@philippinedev.com>
2016-03-19 00:28:25 +0800

    added settings to deploy app

---
+++ config/deploy.rb
@@ -0,0 +1,56 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'testdeploy'
+set :repo_url, 'git@example.com:rusbal/test-cap-deploy.git'
+set :deploy_to, '/home/deployer/testdeploy'
+set :user, 'deployer'
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets}
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  %w[start stop restart].each do |command|
+    desc 'Manage Unicorn'
+    task command do
+      on roles(:app), in: :sequence, wait: 1 do
+        execute "/etc/init.d/unicorn_#{fetch(:application)} #{command}"
+      end
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
